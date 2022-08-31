244d3819c0950eb601eff856e9df07adc96beb2d
berkkosal <kosalnberk@gmail.com>
2022-08-31 16:18:49 +0300

    deneme commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+
+        int berk = 5;
+        String aloalo = "Wow";
+        System.out.printf("dsadsa");
         System.out.println("Hello world!");
     }
+
+
+
+
+
+
+
+
 
 
 
