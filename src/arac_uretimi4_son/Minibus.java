e2b7b988d6b55a1a82aa559277288172d6eea645
berkkosal <kosalnberk@gmail.com>
2022-09-26 19:19:16 +0300

    fabrika sorusu

--- src/arac_uretimi4_son/Minibus.java
+++ src/arac_uretimi4_son/Minibus.java
@@ -5,19 +5,25 @@ abstract class Minibus extends Tasit {
     private static int uretilenKamyonSayisi;
 
     public Minibus(String modelAdi, int motorHacmi) {
-        super(modelAdi, motorHacmi, 160,"Klimalı");
-        uretilenKamyonSayisi ++;
+        super(modelAdi, motorHacmi, 160, "Klimalı");
+        uretilenKamyonSayisi++;
 
 
     }
 
     @Override
     public void gazaBas() {
+        if (!isCalisiyorMu()) return;
+        setoAnkiHiz(getoAnkiHiz() + 10);
+        System.out.println("Anlık hızınız: " + getoAnkiHiz());
 
     }
 
     @Override
     public void freneBas() {
-
+        if (isCalisiyorMu()) {
+            setoAnkiHiz(getoAnkiHiz() - 10);
+            System.out.println("Anlık hızınız: " + getoAnkiHiz());
+        }
     }
 }
