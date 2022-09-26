5ea1173fe91c20e48a9936c8198e59688e39a81b
berkkosal <kosalnberk@gmail.com>
2022-09-26 20:19:56 +0300

    engin demirog odevler

---
+++ src/tum_edemirog/AsalSayi.java
@@ -0,0 +1,30 @@
+package tum_edemirog;
+
+public class AsalSayi {
+
+    public static void main(String[] args) {
+        int number = 2;
+        boolean isPrime = true;
+
+        if(number==1){
+            System.out.println("Sayı asal değildir.");
+            return;
+        }
+        if (number<1){
+            System.out.println("Geçersiz sayı.");
+            return;
+        }
+
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                isPrime = false;
+            }
+        }
+
+        if (isPrime){
+            System.out.println("Sayınız asaldır.");
+
+        } else System.out.println("Sayınız asal değildir.");
+
+    }
+}
