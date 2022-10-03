e152e02be091472f55274f7056cf4bb6a3e6d377
berkkosal <kosalnberk@gmail.com>
2022-10-03 19:16:08 +0300

    basarisizlik

---
+++ src/usta_sifu_egzersizler/enbuyuk_ikincisayi/Deneme2.java
@@ -0,0 +1,39 @@
+package usta_sifu_egzersizler.enbuyuk_ikincisayi;
+
+public class Deneme2 {
+
+    public static void main(String[] args) {
+
+
+        int[] sayiDizisi = {942, 23, 532, 44, 85, 12};
+
+
+    }
+
+    public static int enBuyukSayiyiBul(int [] dizi){
+        int enBuyukSayi = 0;
+
+        for (int i = 0; i <  dizi.length; i++ ){
+            if (dizi[i] <  )
+        }
+
+
+
+
+
+
+        return enBuyukSayi;
+    }
+
+    public static int enBuyukIkinciSayiyiBul(int[] dizi) {
+        int enBuyukIkinciSayi = 0;
+
+        for (int i = 0; i < dizi.length; i++) {
+
+
+
+        }
+        return enBuyukIkinciSayi;
+    }
+
+}
